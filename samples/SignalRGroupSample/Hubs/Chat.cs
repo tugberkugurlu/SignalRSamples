a83198c1e0f4dbad71498f49362479c648c0b024
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-03-14 21:16:55 +0200

    initial commit

---
+++ samples/SignalRGroupSample/Hubs/Chat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace SignalRGroupSample.Hubs {
+
+    public class Chat : Hub {
+
+        public void Send(string data) {
+
+            Clients.addMessage(data, this.Context.ConnectionId);
+        }
+
+        public void Send(string data, string room) {
+
+            Clients[room].addMessage(data, this.Context.ConnectionId.ToString() + "on room: " + room);
+        }
+
+        public void JoinRoom(string room) {
+
+            AddToGroup(room);
+            Clients[room].announceNewcommer(this.Context.ConnectionId);
+        }
+    }
+}
