4f0041a4a6d256407cb9503432b09eb4e6374f46
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-11 18:29:59 +0300

    added the MoveShapeDemo sample

---
+++ samples/SignalRGroupSample.ServerClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SignalR.Client.Hubs;
+
+namespace SignalRGroupSample.ServerClient {
+    class Program {
+
+        static void Main(string[] args) {
+
+            var connection = new HubConnection("http://localhost:9669/");
+
+            var myHub = connection.CreateProxy("SignalRGroupSample.Hubs.Chat");
+            
+            myHub.On("addMessage", data => Console.WriteLine(data));
+
+            connection.Start().Wait();
+
+            myHub.Invoke("Send", "Hi from server...").ContinueWith(task => {
+                if (task.IsFaulted) {
+                    Console.WriteLine("An error occurred during the method call {0}", task.Exception.GetBaseException());
+                } else {
+                    Console.WriteLine("Successfully called MethodOnServer");
+                }
+            });
+
+            Console.WriteLine("connection started...");
+            Console.ReadLine();
+        }
+    }
+}
