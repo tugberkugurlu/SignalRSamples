298920295b5e15e1509bce5f9acbb3bb8c94aff7
tugberkugurlu <tugberkugurlu@gmail.com>
2013-04-18 18:06:26 +0300

    added pp

---
+++ ConnectionMappingSample/ConnectionMappingSample/Infrastructure/SafeCollection.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConnectionMappingSample.Infrastructure {
+    public class SafeCollection {
+    }
+}
