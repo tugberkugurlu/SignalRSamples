bdb58d32196588e598be7f4c1f4c00a75d96822d
tugberkugurlu <tugberkugurlu@gmail.com>
2013-04-18 15:26:01 +0300

    Upgraded to SignalR 1.0.1

--- ConnectionMappingSample/ConnectionMappingSample/Hubs/ChatHub.cs
+++ ConnectionMappingSample/ConnectionMappingSample/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.SignalR.Hubs;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
