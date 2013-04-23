bc468ef03a03fbb3366021b5cff4eb25a0c75ca6
tugberkugurlu <tugberkugurlu@gmail.com>
2013-04-23 11:42:44 +0300

    updated it to use Microsoft.AspNet.SignalR 1.1.0-beta1

--- ConnectionMappingSample/ConnectionMappingSample/Hubs/ChatHub.cs
+++ ConnectionMappingSample/ConnectionMappingSample/Hubs/ChatHub.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNet.SignalR;
-using Microsoft.AspNet.SignalR.Hubs;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace ConnectionMappingSample.Hubs {
 
