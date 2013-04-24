861bc54dc0675f2c49d0998b57929f4e18e39862
tugberkugurlu <tugberkugurlu@gmail.com>
2013-04-24 09:08:10 +0300

    added EF entities and migrations

---
+++ MultiLayerSignalRSample/MultiLayerSignalRSample.Domain/Entities/PrivateChatMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiLayerSignalRSample.Domain.Entities {
+
+    public class PrivateChatMessage 
+    {
+        public int Id { get; set; }
+        
+        [ForeignKey("Sender")]
+        public int SenderId { get; set; }
+
+        [ForeignKey("Receiver")]
+        public int ReceiverId { get; set; }
+
+        public string Content { get; set; }
+        public DateTimeOffset ReceivedOn { get; set; }
+
+        [InverseProperty("SenderPrivateChatMessages")]
+        public User Sender { get; set; }
+
+        [InverseProperty("ReceiverPrivateChatMessages")]
+        public User Receiver { get; set; }
+    }
+}
--- MultiLayerSignalRSample/MultiLayerSignalRSample.Domain/Entities/User.cs
+++ MultiLayerSignalRSample/MultiLayerSignalRSample.Domain/Entities/User.cs
@@ -1,4 +1,5 @@
 ﻿using GenericRepository;
+using System.Collections.Generic;
 
 namespace MultiLayerSignalRSample.Domain.Entities
 {
@@ -6,5 +7,10 @@ namespace MultiLayerSignalRSample.Domain.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+
+        public ICollection<ChatMessage> ChatMessages { get; set; }
+        public ICollection<PrivateChatMessage> SenderPrivateChatMessages { get; set; }
+        public ICollection<PrivateChatMessage> ReceiverPrivateChatMessages { get; set; }
+        public ICollection<HubConnection> HubConnections { get; set; }
     }
 }
