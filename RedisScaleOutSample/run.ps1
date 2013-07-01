459237701d9390c75b876a1b9194e4529a53cd11
tugberkugurlu <tugberkugurlu@gmail.com>
2013-07-01 17:51:49 +0300

    clever way to find the program files folder

--- RedisScaleOutSample/run.ps1
+++ RedisScaleOutSample/run.ps1
@@ -1,6 +1,18 @@
+function programfiles-dir {
+    if (is64bit -eq $true) {
+        (Get-Item "Env:ProgramFiles(x86)").Value
+    } else {
+        (Get-Item "Env:ProgramFiles").Value
+    }
+}
+
+function is64bit() {
+    return ([IntPtr]::Size -eq 8)
+}
+
 $executingPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 $appPPath = (join-path $executingPath "RedisScaleOutSample")
-$iisExpress = "c:\Program Files (x86)\IIS Express\iisexpress.exe"
+$iisExpress = "$(programfiles-dir)\IIS Express\iisexpress.exe"
 $args1 = "/path:$appPPath /port:9090 /clr:v4.0"
 $args2 = "/path:$appPPath /port:9091 /clr:v4.0"
 
